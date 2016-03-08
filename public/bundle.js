6345c7aa393e850983d4018e5ffd2990b9da0502
Drew <Drew@users-MacBook-Pro-1055.local>
2016-03-08 11:09:54 -0800

    mostly ready

--- public/bundle.js
+++ public/bundle.js
@@ -32,91 +32,6 @@ angular.module('optionChainDemo', [
         }
       })
 
-angular.module('optionChainDemo')
-       .controller('ChainCtrl', function($scope, $state, $firebaseObject ){
-         $scope.currentSymbol = $state.params.stockSymbol
-           $scope.chainRef = new Firebase('https://rooftoptrading.firebaseio.com/market/' + $scope.currentSymbol + '/currentChains')
-           $scope.data = $firebaseObject($scope.chainRef)
-
-
-       })
-
-angular.module('optionChainDemo')
-      .directive('optionChain', function($firebaseArray, $state){
-        return {
-          scope: {
-            chainData : "@"
-          },
-          templateUrl: "views/chain/optionChain.html",
-          link: function(scope, el, attrs){
-            scope.symbol = $state.params.stockSymbol
-
-            scope.$watch(function(){return attrs.chainData}, function(n,o){
-              scope.chainData = angular.fromJson(n)
-              console.log('change!');
-              if(n){
-                if(scope.chainData['2016-03-18']){
-                  for(var exp in scope.chainData){
-                    try{
-                      var chain = []
-                      for(strike in scope.chainData[exp].chain){
-                        chain.push(scope.chainData[exp].chain[strike])
-                      }
-                      scope.chainData[exp].chain = chain
-                      scope.expirations = Object.keys(scope.chainData)
-                      scope.expirations.splice(0,2)
-                      scope.chainsLoaded = true;
-
-                      if(scope.currentlyDisplayingChain){
-                        scope.currentlyDisplayingChain = scope.chainData[scope.isDisplayingThisExpiration].chain
-                      }
-                    } catch(e){}
-                  }
-                }
-              }
-            })
-
-          $('body').on('click', '.dropdown',showUL)
-
-          function showUL(e){
-             $(e.target).closest('th').find('.dropdown-menu').toggle()
-          }
-
-          scope.displayThisExpiration = function(exp){
-            scope.isDisplayingThisExpiration = exp;
-            scope.currentlyDisplayingChain = scope.chainData[exp].chain
-          }
-
-          scope.Math = window.Math
-          scope.dataShown = [{title:"Open Interest", key:"open_interest"}, {title:"High", key: "high"}]
-
-
-          scope.addToDataShown = function(index, data, type){
-            type=="call" ? insertRegular() : insertReverse()
-            function insertRegular(){
-                scope.dataShown.splice(index, 1, {title: data, key: scope.Objectify(data)})
-            }
-            function insertReverse(){
-                scope.dataShown.splice(scope.dataShown.length -1 -index, 1, {title: data, key: scope.Objectify(data)});
-            }
-          }
-
-          scope.headers = {
-            greeks: ["Delta", "Theta", "Vega", "Gamma"],
-            price: ["High", "Low" ],
-            volatility: ["IV"],
-            expiration: "Expiration"
-          }
-
-
-
-          scope.Objectify = function(title){
-            return title.toLowerCase().replace(' ',"_")
-           }
-          }
-        }
-      })
-
 'use strict'
 angular.module('optionChainDemo')
       .controller('HomeCtrl', function($scope, $firebaseArray, marketArray, $state){
@@ -1020,4 +935,89 @@ angular.module('optionChainDemo')
            Sector: "Currency ETF"
            }
            ]
+      })
+
+angular.module('optionChainDemo')
+       .controller('ChainCtrl', function($scope, $state, $firebaseObject ){
+         $scope.currentSymbol = $state.params.stockSymbol
+           $scope.chainRef = new Firebase('https://rooftoptrading.firebaseio.com/market/' + $scope.currentSymbol + '/currentChains')
+           $scope.data = $firebaseObject($scope.chainRef)
+
+
+       })
+
+angular.module('optionChainDemo')
+      .directive('optionChain', function($firebaseArray, $state){
+        return {
+          scope: {
+            chainData : "@"
+          },
+          templateUrl: "views/chain/optionChain.html",
+          link: function(scope, el, attrs){
+            scope.symbol = $state.params.stockSymbol
+
+            scope.$watch(function(){return attrs.chainData}, function(n,o){
+              scope.chainData = angular.fromJson(n)
+              console.log('change!');
+              if(n){
+                if(scope.chainData['2016-03-18']){
+                  for(var exp in scope.chainData){
+                    try{
+                      var chain = []
+                      for(strike in scope.chainData[exp].chain){
+                        chain.push(scope.chainData[exp].chain[strike])
+                      }
+                      scope.chainData[exp].chain = chain
+                      scope.expirations = Object.keys(scope.chainData)
+                      scope.expirations.splice(0,2)
+                      scope.chainsLoaded = true;
+
+                      if(scope.currentlyDisplayingChain){
+                        scope.currentlyDisplayingChain = scope.chainData[scope.isDisplayingThisExpiration].chain
+                      }
+                    } catch(e){}
+                  }
+                }
+              }
+            })
+
+          $('body').on('click', '.dropdown',showUL)
+
+          function showUL(e){
+             $(e.target).closest('th').find('.dropdown-menu').toggle()
+          }
+
+          scope.displayThisExpiration = function(exp){
+            scope.isDisplayingThisExpiration = exp;
+            scope.currentlyDisplayingChain = scope.chainData[exp].chain
+          }
+
+          scope.Math = window.Math
+          scope.dataShown = [{title:"Open Interest", key:"open_interest"}, {title:"High", key: "high"}]
+
+
+          scope.addToDataShown = function(index, data, type){
+            type=="call" ? insertRegular() : insertReverse()
+            function insertRegular(){
+                scope.dataShown.splice(index, 1, {title: data, key: scope.Objectify(data)})
+            }
+            function insertReverse(){
+                scope.dataShown.splice(scope.dataShown.length -1 -index, 1, {title: data, key: scope.Objectify(data)});
+            }
+          }
+
+          scope.headers = {
+            greeks: ["Delta", "Theta", "Vega", "Gamma"],
+            price: ["High", "Low" ],
+            volatility: ["IV"],
+            expiration: "Expiration"
+          }
+
+
+
+          scope.Objectify = function(title){
+            return title.toLowerCase().replace(' ',"_")
+           }
+          }
+        }
       })
